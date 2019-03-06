fface88cd42c72d215c2b191f4f4cbc98cf5b6e5
神经病医生 <admin@example.com>
2019-03-06 21:19:06 +0800

    添加维护视图

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', '千比特短信平台'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://smsplatform.test'),
 
     'asset_url' => env('ASSET_URL', null),
 
